b8758332dd362a93eaab9e2b5e5659fe6af02ef2
LengaTakamura <lengatakamuu6200@gmail.com>
2025-12-11 20:20:43 +0900

    demo

--- Assets/Demo.cs
+++ Assets/Demo.cs
@@ -28,7 +28,7 @@ public class Demo : MonoBehaviour
     {
         myRigidbody = GetComponent<Rigidbody>();
         myRigidbody.AddForce(0, -9.81f, 0);
-        myTransform.position = new Vector3(0, 5, 0);
+        myTransform.position = new Vector3(0, 5, 1001101001);
         
     }
 
